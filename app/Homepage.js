e43bea7db11c3b124e6bdb3a65c15cef1b8a49f3
Vinay Wasnik <127746998+VinayWasnik@users.noreply.github.com>
2023-08-21 10:07:56 +0530

    Create Homepage.js

---
+++ app/Homepage.js
@@ -0,0 +1,12 @@
+// pages/Homepage.js
+import React from 'react';
+
+function Homepage() {
+  return (
+    <div>
+      <h1>First Page of Next JS</h1>
+    </div>
+  );
+}
+
+export default Homepage;
